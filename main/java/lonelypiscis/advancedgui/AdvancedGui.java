db1e793f87b88a6d6b536eb4e653ac9813bdf0e2
lonelypiscis <gurokrei@gmail.com>
2014-06-03 21:13:37 +0200

    Added new Message

--- main/java/lonelypiscis/advancedgui/AdvancedGui.java
+++ main/java/lonelypiscis/advancedgui/AdvancedGui.java
@@ -1,20 +1,20 @@
-package com.example.examplemod;
+package lonelypiscis.advancedgui;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = AdvancedGui.MODID, version = AdvancedGui.VERSION)
+public class AdvancedGui
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "AdvancedGui";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
 		// some example code
-        System.out.println("I am a Example Mod!");
+        System.out.println("I am a Example Modsfd!");
     }
 }
